05e534cfc7dd307267efa991164e1427d38e4da2
alfre755 <4lfr3755@gmail.com>
2026-03-27 02:28:09 -0300

    Feat: Pages
    Se agregan pages de login y sign up

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,18 +1,22 @@
-import { ThemeLogo } from "@/components/shared/theme-logo";
-import LoginForm from "@/modules/login/forms/login-form";
+import SignUpForm from "@/modules/login/forms/sign-up-form";
 import Image from "next/image";
 
 export default function page() {
   return (
     <div className="flex flex-col p-2 gap-4 min-h-screen">
       <div className="flex justify-center">
-        <ThemeLogo width={250} height={250} />
+        <Image
+          src="/assets/images/logo_negro.png"
+          alt="Logo"
+          width={250}
+          height={250}
+        />
       </div>
       <div className="text-2xl font-bold text-center">
         Ingresa a nuestra plataforma
       </div>
       <div className="mx-auto w-full max-w-md ">
-        <LoginForm />
+        <SignUpForm />
       </div>
     </div>
   );
