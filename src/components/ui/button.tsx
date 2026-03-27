b3085373f86a6cb2168e385df705ed2959dd0b74
alfre755 <4lfr3755@gmail.com>
2026-03-27 02:24:21 -0300

    Feat: Components shadcn
    Se agfregan componente shadcn

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
           "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
         link: "text-primary underline-offset-4 hover:underline",
         "link-light":
-          "text-primary bg-white underline-offset-4 hover:text-primary/80 transition hover:underline",
+          "text-primary underline-offset-4 hover:text-primary/80 transition hover:underline",
       },
       size: {
         default:
