dd44cce47ad12fa449bffe1e007a5c6f568539b6
alfre755 <4lfr3755@gmail.com>
2026-04-02 02:53:02 -0300

    Feat: Page users
    Se agrega la estructura inicial de users

--- src/app/(app)/users/page.tsx
+++ src/app/(app)/users/page.tsx
@@ -1,3 +1,32 @@
-export default function page() {
-  return <div></div>;
+import { columns } from "../../../modules/users/_components/columns";
+import { DataTable } from "@/components/shared/data-table";
+import { HeaderPages } from "@/components/shared/header-pages";
+import { Button } from "@/components/ui/button";
+import { getUsers } from "@/modules/users/queries";
+
+export default async function page() {
+  const users = await getUsers();
+  return (
+    <>
+      <header>
+        <HeaderPages
+          title="Usuarios"
+          description="Gestiona los usuarios registrados en la plataforma."
+          breadcrumbs={[
+            { label: "Dashboard", href: "/dashboard" },
+            { label: "Usuarios" },
+          ]}
+        />
+      </header>
+      <div className="container mx-auto">
+        <DataTable
+          columns={columns}
+          data={users}
+          searchKey="email"
+          searchPlaceholder="Filtrar emails..."
+          action={<Button size="sm">Agregar Usuario</Button>}
+        />
+      </div>
+    </>
+  );
 }
