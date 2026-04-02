d9bcb6d2c14dd86f194256a70ae9619b64712fa3
alfre755 <4lfr3755@gmail.com>
2026-04-02 02:53:40 -0300

    Feat: Dioalog app
    Se crea componente para manejar los dialog ( popups) dentro de la platafroma

---
+++ src/components/shared/app-dialog.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+interface AppDialogProps {
+  title: string;
+  trigger: React.ReactNode;
+  children: React.ReactNode;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+}
+
+export function AppDialog({
+  title,
+  trigger,
+  children,
+  open,
+  onOpenChange,
+}: AppDialogProps) {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+}
