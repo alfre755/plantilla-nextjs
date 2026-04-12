8577e89dfc9d8492de511872dcd1d42c4c74ae89
alfre755 <4lfr3755@gmail.com>
2026-04-12 17:55:39 -0400

    Feat: Types

--- src/modules/users/types.ts
+++ src/modules/users/types.ts
@@ -1,13 +1,21 @@
 import { user } from "@/db/schema";
+import { UserWithRole } from "better-auth/plugins";
 import { InferSelectModel, InferInsertModel } from "drizzle-orm";
 
-type User = InferSelectModel<typeof user>;
+type User = UserWithRole;
 
 type CreateUser = {
   email: string;
   password: string;
   name: string;
   role: "admin" | "user";
+};
+
+export type EditUser = {
+  name?: string;
+  email?: string;
+  role?: "admin" | "user";
+  password?: string;
 };
 
 export default User;
