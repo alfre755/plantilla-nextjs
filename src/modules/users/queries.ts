648a09444c1fb6c53f67b88682f4cda9b03f240e
alfre755 <4lfr3755@gmail.com>
2026-04-12 17:55:50 -0400

    Refactor: Query to betterauth

--- src/modules/users/queries.ts
+++ src/modules/users/queries.ts
@@ -1,8 +1,18 @@
 import { db } from "@/db";
 import { user } from "@/db/schema";
+import { auth } from "@/lib/auth";
 import { desc } from "drizzle-orm";
+import { headers } from "next/headers";
 
 export async function getUsers() {
-  const users = await db.select().from(user).orderBy(desc(user.createdAt));
+  const users = await auth.api.listUsers({
+    query: {
+      limit: 500,
+      offset: 0,
+      sortBy: "createdAt",
+      sortDirection: "desc",
+    },
+    headers: await headers(),
+  });
   return users;
 }
