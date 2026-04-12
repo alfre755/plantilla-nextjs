e4841286466e554732302022c9fa052369f7216b
alfre755 <4lfr3755@gmail.com>
2026-04-12 17:56:11 -0400

    Feat: Actions
    Se agregan actions para editar usuario y sacar el usuario por id

--- src/modules/users/actions.ts
+++ src/modules/users/actions.ts
@@ -2,7 +2,8 @@
 
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
-import { CreateUser } from "./types";
+import { CreateUser, EditUser } from "./types";
+import { revalidatePath } from "next/cache";
 
 export async function createUser(data: CreateUser) {
   const result = await auth.api.createUser({
@@ -18,4 +19,60 @@ export async function createUser(data: CreateUser) {
   if (!result) throw new Error("Error al crear usuario");
 
   return result;
+}
+
+export async function editUser(userId: string, data: EditUser) {
+  // Actualiza nombre y email
+  await auth.api.adminUpdateUser({
+    body: {
+      userId,
+      data: {
+        ...(data.name && { name: data.name }),
+        ...(data.email && { email: data.email }),
+      },
+    },
+    headers: await headers(),
+  });
+
+  // Cambia rol por separado
+  if (data.role) {
+    await auth.api.setRole({
+      body: { userId, role: data.role },
+      headers: await headers(),
+    });
+  }
+
+  // Cambia password por separado
+  if (data.password) {
+    await auth.api.setUserPassword({
+      body: { userId, newPassword: data.password },
+      headers: await headers(),
+    });
+  }
+
+  revalidatePath("/users");
+}
+
+export async function deleteUser(userId: string) {
+  const result = await auth.api.removeUser({
+    body: {
+      userId: userId,
+    },
+
+    headers: await headers(),
+  });
+
+  if (!result) throw new Error("Error al eliminar usuario");
+  revalidatePath("/users");
+
+  return result;
+}
+
+// modules/users/queries.ts
+export async function getUserById(id: string) {
+  const user = await auth.api.getUser({
+    query: { id },
+    headers: await headers(),
+  });
+  return user;
 }
