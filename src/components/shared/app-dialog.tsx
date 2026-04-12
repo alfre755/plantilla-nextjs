70f1f2c093fe546ade0fdc68b33805c31c20b417
alfre755 <4lfr3755@gmail.com>
2026-04-12 17:29:55 -0400

    Fix: Trigger de adialog

--- src/components/shared/app-dialog.tsx
+++ src/components/shared/app-dialog.tsx
@@ -10,7 +10,7 @@ import {
 
 interface AppDialogProps {
   title: string;
-  trigger: React.ReactNode;
+  trigger?: React.ReactNode;
   children: React.ReactNode;
   open?: boolean;
   onOpenChange?: (open: boolean) => void;
@@ -25,7 +25,7 @@ export function AppDialog({
 }: AppDialogProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      {trigger && <DialogTrigger asChild>{trigger}</DialogTrigger>}
       <DialogContent>
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
