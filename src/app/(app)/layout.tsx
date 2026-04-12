383339687b4f09dd92c0640fab78e8c4359f79aa
alfre755 <4lfr3755@gmail.com>
2026-04-12 17:57:48 -0400

    Feat: Session

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,22 +1,19 @@
 // app/(app)/layout.tsx
-import { AppSidebar } from "@/components/shared/app-sidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/shared/sidebar/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
-import { NavRole } from "@/config/navigation";
-import { ThemeProvider } from "@/components/shared/theme-provider";
 
 export default async function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const session = await auth.api.getSession({ headers: await headers() });
-  const role = "admin" as NavRole;
+  const session = await auth.api.getSession({ headers: await headers() });
 
   return (
     <SidebarProvider className="h-svh overflow-hidden">
-      <AppSidebar role={role} />
+      <AppSidebar session={session} />
       <main className="flex flex-col flex-1 min-w-0 min-h-0 overflow-hidden">
         <div className="flex flex-col flex-1 min-h-0 px-4 py-4">{children}</div>
       </main>
